c786951c12b865887eced5df24929cc883954e5a
Makoveychyk <makoveychyk@gmail.com>
2026-01-20 10:43:56 +0200

    Add env config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `.env.${process.env.NODE_ENV || 'local'}`,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
